cf11e9806a55fab9648b25d13c077ae3a6bb409f
Evan Mann <ejmann900@gmail.com>
2024-02-01 17:26:22 -0700

    changed the names of files and removed unused files

--- ex2.5.py
+++ ex2.5.py
@@ -1,27 +1,49 @@
-f = open("pg2701.txt", "r", encoding="utf-8")
-f.seek(843, 0)
-numVowels = []
-for line in f:
-    words = line.split(' ')
-    for word in words:
-        curVowels = 0
-        curVowels += word.count('a')
-        curVowels += word.count('e')
-        curVowels += word.count('i')
-        curVowels += word.count('o')
-        curVowels += word.count('u')
-        curVowels += word.count('y')
-        curVowels += word.count('A')
-        curVowels += word.count('E')
-        curVowels += word.count('I')
-        curVowels += word.count('O')
-        curVowels += word.count('U')
-        curVowels += word.count('Y')
-        numVowels.append(curVowels)
+import json
+import collections.abc
+import timeit
 
-sum = 0
-for num in numVowels:
-    sum += num
 
-average = sum / len(numVowels)
-print(average)
+
+setupcode = '''
+import json
+import collections.abc
+file = open('large-file.json', 'r', encoding='utf-8')
+data = json.load(file)
+def check(dictionary):
+    global counter
+    for record in dictionary.keys():
+        if record == "size":
+            dictionary["size"] = 35
+        elif isinstance(dictionary[record], dict):
+            check(dictionary[record])
+        elif isinstance(dictionary[record], collections.abc.Sequence):
+            for item in dictionary[record]:
+                if isinstance(item, dict):
+                    check(item)
+def forloop(dat):
+    for ary in dat:
+        check(ary)'''
+
+
+
+
+time = timeit.repeat(setup=setupcode, stmt='forloop(data)', repeat = 10, number = 1)
+print(time)
+total = 0
+for x in time:
+    total += x
+print("average execution time is", total/10)
+#for record in data:
+#   record["size"] = 35
+#   if isinstance(record, dict):
+
+#for ary in data:
+#    check(ary)
+file = open('large-file.json', 'r', encoding='utf-8')
+data = json.load(file)
+
+
+output_data = data[::-1]
+with open("output.2.3.json", "w") as output_file:
+    json.dump(output_data, output_file, indent=2)
+
